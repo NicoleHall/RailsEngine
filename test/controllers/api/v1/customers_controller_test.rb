fac8c814dc28b7d9bf9ea8cab8d9b962aaadebf7
Nicole Hall <nicolealexandrahall@gmail.com>
2016-02-24 09:45:53 -0700

    show and index actions for customers controller pass

---
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+
+class Api::V1::CustomersControllerTest < ActionController::TestCase
+
+  test '#index' do
+
+    get :index, format: :json
+    customers = JSON.parse(response.body)
+    first_customer = customers.last
+    assert_response :success
+    assert_equal 'Grayce', first_customer['first_name']
+    assert_equal 'Hettinger', first_customer['last_name']
+  end
+
+
+  test '#show' do
+
+    get :show, format: :json, id: 56
+    customer = JSON.parse(response.body, symbolize_names: true)
+    assert_response :success
+    assert_equal "Eleazar", customer[:first_name]
+    assert_equal "Wisozk", customer[:last_name]
+
+  end
+end
