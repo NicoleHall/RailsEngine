dd8ef6cc34d4ce1e148e48ca3ff20971be93e996
Nicole Hall <nicolealexandrahall@gmail.com>
2016-02-24 12:31:57 -0700

    fixed symbols on the customers controller show test

--- test/controllers/api/v1/customers_controller_test.rb
+++ test/controllers/api/v1/customers_controller_test.rb
@@ -4,10 +4,10 @@ require 'test_helper'
 class Api::V1::CustomersControllerTest < ActionController::TestCase
 
   test '#index' do
-
     get :index, format: :json
     customers = JSON.parse(response.body)
     first_customer = customers.last
+
     assert_response :success
     assert_equal 'Nicole', first_customer['first_name']
     assert_equal 'Murray', first_customer['last_name']
@@ -15,12 +15,11 @@ class Api::V1::CustomersControllerTest < ActionController::TestCase
 
 
   test '#show' do
-
     get :show, format: :json, id: 56
-    customer = JSON.parse(response.body, symbolize_names: true)
-    assert_response :success
-    assert_equal "Eleazar", customer[:first_name]
-    assert_equal "Wisozk", customer[:last_name]
+    customer = JSON.parse(response.body)
 
+    assert_response :success
+    assert_equal "Eleazar", customer["first_name"]
+    assert_equal "Wisozk", customer["last_name"]
   end
 end
