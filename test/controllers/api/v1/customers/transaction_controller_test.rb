99ccda9097a09c0b5de1050e4dece1aef6c6fb6e
Nicole Hall <nicolealexandrahall@gmail.com>
2016-02-25 09:33:50 -0700

    this test is passing test/controllers/api/v1/customers/transaction_controller_test.rb

--- test/controllers/api/v1/customers/transaction_controller_test.rb
+++ test/controllers/api/v1/customers/transaction_controller_test.rb
@@ -6,11 +6,15 @@ class Api::V1::Customers::TransactionsControllerTest < ActionController::TestCas
   test '#index' do
     get :index, format: :json, customer_id: 55
     assert_response :success
-    alisha = JSON.parse(response.body)
+    alishas_transactions = JSON.parse(response.body)
 
- 
-    alisha.transactions
-    require 'pry'; binding.pry
+    assert_equal 4, alishas_transactions.size
+    assert_equal 279, alishas_transactions.first["id"]
+    assert_equal 246, alishas_transactions.first["invoice_id"]
+    assert_equal "4233739940920683",  alishas_transactions.first["credit_card_number"]
+    assert_equal "success", alishas_transactions.first["result"]
+
+
   end
 
   end
